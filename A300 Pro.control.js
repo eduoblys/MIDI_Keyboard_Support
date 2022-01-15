efd5e086f1b62033a572e010f40a60b9f005e616
eduoblys <eduoblys@gmail.com>
2022-01-15 13:19:22 +0100

    Both Inputs are working and producing sound in Bitwig. Also logging in console

--- A300 Pro.control.js
+++ A300 Pro.control.js
@@ -5,81 +5,78 @@ loadAPI(15);
 host.setShouldFailOnDeprecatedUse(true);
 
 host.defineController("Roland", "A300 Pro", "0.2", "08b8ef3d-73f7-4667-8e79-21f1314a2fe5", "Ega420");
-
 host.defineMidiPorts(2, 1);
+const MOXF_BUTTON_PLAY   = 0x5E;
+const MOXF_BUTTON_STOP   = 0x5D;
+const MOXF_BUTTON_REC    = 0x5F;
+const MOXF_BUTTON_FWD    = 0x5C;
+const MOXF_BUTTON_BACK   = 0x5B;
+const MOXF_BUTTON_LOCATE = 0x58;
 
-if (host.platformIsWindows())
+var transport = null;
+
+
+function init()
 {
-   // TODO: Set the correct names of the ports for auto detection on Windows platform here
-   // and uncomment this when port names are correct.
-   // host.addDeviceNameBasedDiscoveryPair(["Input Port 0", "Input Port 1"], ["Output Port 0", "Output Port -1"]);
+   var inputPort = host.getMidiInPort(0);
+   inputPort.setMidiCallback(onMidi0);
+
+   noteIn = inputPort.createNoteInput("Yamaha MOXF", "8?????", "9?????", "B?01??", "B?40??", "E?????");
+
+   var noteMap = initArray (-1, 128);
+   for (i = 0; i < 128; i++)
+      noteMap[i] = 127 - i;
+   noteIn.setKeyTranslationTable (noteMap);
+
+   transport = host.createTransport ();
+
+   println("MOXF initialized!");
 }
-else if (host.platformIsMac())
+
+function onMidi0(status, data1, data2)
 {
-   // TODO: Set the correct names of the ports for auto detection on Mac OSX platform here
-   // and uncomment this when port names are correct.
-   // host.addDeviceNameBasedDiscoveryPair(["Input Port 0", "Input Port 1"], ["Output Port 0", "Output Port -1"]);
-}
-else if (host.platformIsLinux())
-{
-   // TODO: Set the correct names of the ports for auto detection on Linux platform here
-   // and uncomment this when port names are correct.
-   // host.addDeviceNameBasedDiscoveryPair(["Input Port 0", "Input Port 1"], ["Output Port 0", "Output Port -1"]);
-}
+   if (isNoteOn(status) && data2 == 127)
+   {
+      switch (data1)
+      {
+         case MOXF_BUTTON_PLAY:
+            transport.play ();
+            break;
 
-function init() {
-   transport = host.createTransport();
-   host.getMidiInPort(0).setMidiCallback(onMidi0);
-   host.getMidiInPort(0).setSysexCallback(onSysex0);
-   host.getMidiInPort(1).setMidiCallback(onMidi1);
-   host.getMidiInPort(1).setSysexCallback(onSysex1);
+         case MOXF_BUTTON_STOP:
+            transport.stop ();
+            break;
 
-   // TODO: Perform further initialization here.
-   println("A300 Pro initialized!!!!!");
-}
+         case MOXF_BUTTON_REC:
+            transport.record ();
+            break;
 
-// Called when a short MIDI message is received on MIDI input port 0.
-function onMidi0(status, data1, data2) {
-   // TODO: Implement your MIDI input handling code here.
-   printMidi(status, data1, data2);
-}
+         case MOXF_BUTTON_FWD:
+            transport.fastForward ();
+            break;
 
-// Called when a MIDI sysex message is received on MIDI input port 0.
-function onSysex0(data) {
-   // MMC Transport Controls:
-   switch (data) {
-      case "f07f7f0605f7":
-         transport.rewind();
-         break;
-      case "f07f7f0604f7":
-         transport.fastForward();
-         break;
-      case "f07f7f0601f7":
-         transport.stop();
-         break;
-      case "f07f7f0602f7":
-         transport.play();
-         break;
-      case "f07f7f0606f7":
-         transport.record();
-         break;
+         case MOXF_BUTTON_BACK:
+            transport.rewind ();
+            break;
+
+         case MOXF_BUTTON_LOCATE:
+            transport.tapTempo ();
+            break;
+
+         default:
+            host.errorln ("Command " + data1 + " is not supported.");
+            break;
+      }
    }
-}
-// Called when a short MIDI message is received on MIDI input port 1.
-function onMidi1(status, data1, data2) {
-   // TODO: Implement your MIDI input handling code here.d
-   printMidi(status, data1, data2);
 
 }
 
-// Called when a MIDI sysex message is received on MIDI input port 1.
-function onSysex1(data) {
+function flush()
+{
+   // println("Flush called.");
 }
 
-function flush() {
-   // TODO: Flush any output to your controller here.
-}
-
-function exit() {
-
+function exit()
+{
+   println("Exited!");
 }
